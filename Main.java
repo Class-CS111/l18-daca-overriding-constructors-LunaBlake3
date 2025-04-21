1a0096376e13c608e934d101b1e1a960c338e1ef
LunaBlake3 <lunablaker@gmail.com>
2025-04-21 17:56:37 +0000

    complete

--- Main.java
+++ Main.java
@@ -8,6 +8,13 @@ class Main
 {
   public static void main(String[] args) 
 	{
+    DACArecipient test = new DACArecipient();
 
+    System.out.println("Test: " + test);
+
+    DACArecipient test2 = new DACArecipient("Blake", "Luna-Beltran",
+     "123456789", "USA", 2440539,2440539,2440539, 'M');
+
+     System.out.println("Test #2: "+ test2);
   }
 }
